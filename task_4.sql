a17d452812e8cdc16cb6d51355e992f7415010c2
verydnobl337 <verydnob@yandex.ru>
2026-01-30 07:13:23 -0400

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,21 @@
 /*добавьте сюда запрос для решения задания 4*/
+-- Задание 4. Найти пиццерии с самым большим кол-ом пицц в меню:
+WITH pizza_expanded AS (
+    SELECT
+        r.name AS restaurant_name,
+        p.key AS pizza_name
+    FROM cafe.restaurants r,
+         jsonb_each_text(r.menu -> 'Пицца') AS p(key, value)
+    WHERE r.restaurant_type = 'pizzeria'
+),
+pizza_count AS (
+    SELECT
+        restaurant_name,
+        COUNT(pizza_name) AS pizza_count,
+        DENSE_RANK() OVER (ORDER BY COUNT(pizza_name) DESC) AS rank
+    FROM pizza_expanded
+    GROUP BY restaurant_name
+)
+SELECT restaurant_name, pizza_count
+FROM pizza_count
+WHERE rank = 1;
