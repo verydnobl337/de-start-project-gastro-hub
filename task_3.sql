1bb02d62a02581138ea944a4a9b8e59d8e54556d
verydnobl337 <verydnob@yandex.ru>
2026-01-30 07:13:02 -0400

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,1 +1,10 @@
 /*добавьте сюда запрос для решения задания 3*/
+-- Задание 3. Топ-3 заведения по смене менеджеров:
+SELECT
+    r.name AS restaurant_name,
+    COUNT(*) AS manager_changes
+FROM cafe.restaurant_manager_work_dates rmwd
+JOIN cafe.restaurants r ON r.restaurant_uuid = rmwd.restaurant_uuid
+GROUP BY r.name
+ORDER BY manager_changes DESC
+LIMIT 3;
