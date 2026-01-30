b39d3469927a8589e0a209165ef70e7d5f828799
verydnobl337 <verydnob@yandex.ru>
2026-01-30 07:12:15 -0400

    Update task_1.sql

--- task_1.sql
+++ task_1.sql
@@ -1,1 +1,23 @@
 /*добавьте сюда запрос для решения задания 1*/
+-- Задание 1. Топ-3 заведения по среднему чеку:
+CREATE OR REPLACE VIEW cafe.top_3_restaurants AS
+SELECT
+    restaurant_name,
+    restaurant_type,
+    ROUND(avg_check, 2) AS avg_check
+FROM (
+    SELECT
+        r.name AS restaurant_name,
+        r.restaurant_type,
+        AVG(s.avg_check) AS avg_check,
+        ROW_NUMBER() OVER (
+            PARTITION BY r.restaurant_type
+            ORDER BY AVG(s.avg_check) DESC
+        ) AS rn
+    FROM cafe.sales s
+    JOIN cafe.restaurants r ON r.restaurant_uuid = s.restaurant_uuid
+    GROUP BY
+        r.name,
+        r.restaurant_type
+) t
+WHERE rn <= 3;
